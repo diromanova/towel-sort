aa3e72f66abcb0ade2a00676c09471ba60a38d32
Diana Linkova <di_rome@mail.ru>
2022-07-18 13:16:41 +0400

    feat: update js code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 // You should implement your task here.
 
 module.exports = function towelSort(matrix) {
-    const result = matrix.reduce((acc, cur, i) => {
-        cur.sort((a, b) => (!(i & 1) ? a - b : b - a)).map((e) => acc.push(e));
-        return acc;
-    }, []);
-
-    return result;
+    let newArr = [];
+    if (matrix) {
+        matrix.forEach((item, i) => {
+            if (i % 2) {
+                item.reverse();
+            }
+            newArr = newArr.concat(item);
+        });
+    }
+    return newArr;
 };
